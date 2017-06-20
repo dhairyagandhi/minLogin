54cfd598fcd057e41355a013e63c3d4f026209c0
dgrules <dgdroidace@gmail.com>
2017-06-21 03:33:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/dhairyagandhi/minimallogin/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dhairyagandhi.minimallogin;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
